a9d04f3643b833e1b9be2252caed685340f79e53
Ivan Manylov <ivan.manylow@gmail.com>
2024-03-20 16:18:50 +0300

    1

--- lab3.4.py
+++ lab3.4.py
@@ -5,13 +5,21 @@ number1 = int(random.random()*9)
 number2 = int(random.random()*9)
 answer = int(input(f"{number1} + {number2} = "))
 errors = 0
+goods = 0
 while errors < 3:
     if answer != (number1+number2):
         errors += 1
         print("Ответ неверный")
         if errors == 3:
+            print("Игра окончена")
+            print(f"Правильных ответов: {goods}")
             sys.exit(0)
+        number1 = int(random.random() * 9)
+        number2 = int(random.random() * 9)
         answer = int(input(f"{number1} + {number2} = "))
     else:
+        goods +=1
         print("Правильно!")
-        sys.exit(0)
+        number1 = int(random.random() * 9)
+        number2 = int(random.random() * 9)
+        answer = int(input(f"{number1} + {number2} = "))
