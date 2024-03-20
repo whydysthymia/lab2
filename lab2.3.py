bc471282f2bf68677caec4f0212920bec97fa7ef
Ivan Manylov <ivan.manylow@gmail.com>
2024-03-20 20:04:36 +0300

    1

--- lab2.3.py
+++ lab2.3.py
@@ -1,9 +1,9 @@
-def leapYear(year):
+def leapYear():
+    year = int(input("Введите год:\n"))
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         print(f"Год {year} - високосный")
     else:
         print("Этот год не високосный")
-year = int(input("Введите год:\n"))
-leapYear(year)
+leapYear()
 
 
