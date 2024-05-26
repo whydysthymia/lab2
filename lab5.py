75d2f563184c0a5bcc6a25099813bbf00777694d
Ivan Manylov <ivan.manylow@gmail.com>
2024-05-26 20:10:21 +0300

    1

--- lab5.py
+++ lab5.py
@@ -39,20 +39,22 @@ def ex53():
 
 def ex54():
     myStudents = ["Семенов", "Рогозный", "Манылов", "Ярмолаш", "Федосеева", "Абдурахимов", "Зарецкая", "Болтрукевич", "Бутузова", "Печинская"]
-    '''otherStudents = ["Петров", "Иванов", "Добриков", "Процай", "Силантьева", "Лазарев", "Соменков", "Теплых", "Сорокина", "Макарин"]'''
     otherStudents = ["Петров", "Иванов", "Добриков", "Иванов", "Силантьева", "Иванов", "Соменков", "Иванов", "Сорокина", "Иванов"]
 
     team = []
-    while len(team) != 10:
-        i = random.randint(0, 9)
-        indexI = []
-        if (myStudents[i] not in team) or (myStudents[i] in team and i not in indexI):
+    indexI = []
+    indexJ = []
+    while len(team) != 5:
+        i = random.randint(0,9)
+        if (myStudents[i] not in team) or ((myStudents[i] in team) and (i not in indexI)):
             indexI.append(i)
             team.append(myStudents[i])
-        j = random.randint(0, 9)
-        indexJ = []
-        if (otherStudents[j] not in team) or (otherStudents[j] in team and i not in indexJ):
+    while len(team) != 10:
+        j = random.randint(0,9)
+        if (otherStudents[j] not in team) or ((otherStudents[j] in team) and (j not in indexJ)):
+            indexJ.append(j)
             team.append(otherStudents[j])
+
     team = tuple(sorted(team))
     print(myStudents, "\n", otherStudents)
     print(team, "\n", len(team))
